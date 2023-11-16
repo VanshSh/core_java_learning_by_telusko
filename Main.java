b00a66c8141399d65fed0a9ec196a2bef3e0c3fd
VanshSh <vanshsharma9354@gmail.com>
2023-11-16 22:42:35 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main{
+    public static void main(String[] args) {
+        // Primitive data types
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+
+        // Additional primitive data types
+        byte myByte = 127;        // 8-bit signed integer
+        short myShort = 32767;    // 16-bit signed integer
+        long myLong = 9223372036854775807L;  // 64-bit signed integer
+        double myDouble = 19.99d; // 64-bit double-precision floating-point
+
+        // Reference data type
+        String myText = "Hello";
+
+        // Print variables
+        System.out.println("int: " + myNum);
+        System.out.println("float: " + myFloatNum);
+        System.out.println("char: " + myLetter);
+        System.out.println("boolean: " + myBool);
+        System.out.println("byte: " + myByte);
+        System.out.println("short: " + myShort);
+        System.out.println("long: " + myLong);
+        System.out.println("double: " + myDouble);
+        System.out.println("String: " + myText);
+    }
+}
