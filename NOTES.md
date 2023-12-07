67a2366f3cc415b6b2df5b2dd0c22033213cbe9d
VanshSh <vansh.sharma@seventhtriangle.com>
2023-12-07 23:44:38 +0530

    feat: :sparkles: MultiDimensional array
    Time stamp: 4:13:20

--- NOTES.md
+++ NOTES.md
@@ -84,4 +84,26 @@ OR
 
 int myNum[] = {10, 20, 30, 40};
 
+
+```
+## - Array loop
+
+```
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.println(nums[i][j] + " ");
+            }
+
+        }
+
+        for (int n[] : nums) {
+            for (int m : n) {
+                System.out.println(m + " ");
+            }
+
+        }
+
+
+
 ```
--- ObjectFile.java
+++ ObjectFile.java
@@ -4,20 +4,33 @@ class Calculator {
         int num2 = 56;
         int result = num1 + num2;
         System.out.println(" U_U L-6 in ObjectFile.java => " + result);
-        return (result);
-
+        return result;
     }
-
 }
 
 public class ObjectFile {
     public static void main(String[] args) {
 
         Calculator calc = new Calculator();
-        calc.add();
+        int result = calc.add(); // Save the result in a variable
+        System.out.println("Result from Calculator: " + result);
+
         int[] myNum = { 10, 20, 30, 40 };
+        int[][] twoDArray = new int[3][4]; // 3 rows, 4 columns
 
-        System.out.println(" U_U L-20 in ObjectFile.java => " + myNum[1]);
+        // Initialize the elements in the 2D array
+        for (int i = 0; i < twoDArray.length; i++) {
+            for (int j = 0; j < twoDArray[i].length; j++) {
+                twoDArray[i][j] = i + j; // You can initialize with any values as needed
+            }
+        }
 
+        // Access and print the elements in the 2D array
+        for (int i = 0; i < twoDArray.length; i++) {
+            for (int j = 0; j < twoDArray[i].length; j++) {
+                System.out.print(twoDArray[i][j] + " ");
+            }
+            System.out.println(); // Move to the next row after printing each row
+        }
     }
 }
