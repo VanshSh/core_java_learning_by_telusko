d38cd1f69fe151e628b202efc7b9f9d7d723bd2f
VanshSh <vansh.sharma@seventhtriangle.com>
2023-12-18 23:24:12 +0530

    feat: :sparkles: Time tamp:https://youtu.be/BGTx91t8q50?t=19088

---
+++ Encapsulation.java
@@ -0,0 +1,16 @@
+class Human {
+    int age;
+    private String name;
+}
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        Human obj = new Human();
+
+        obj.age = 22;
+        obj.name = "Vansh Sharma";
+        System.out.println(obj.name);
+        System.out.println(obj.age);
+    }
+
+}
