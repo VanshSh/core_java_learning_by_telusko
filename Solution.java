24f86ae9f6797ec1acc35d3fdaa323d8b630d2d1
VanshSh <vansh.sharma@seventhtriangle.com>
2023-12-21 22:47:26 +0530

    feat:

--- Solution.java
+++ Solution.java
@@ -1,31 +1,36 @@
-import java.util.*;
-import java.text.*;
+import java.util.Scanner;
 
 public class Solution {
 
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
+
+        for (int i = 1; i <= s.length() - k; i++) {
+
+            String currentSubstring = s.substring(i, i + k);
+            if (currentSubstring.compareTo(smallest) < 0) {
+                smallest = currentSubstring;
+            }
+            if (currentSubstring.compareTo(largest) > 0) {
+                largest = currentSubstring;
+            }
+        }
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+
+        return smallest + "\n" + largest;
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double payment = scanner.nextDouble();
-        scanner.close();
+        Scanner scan = new Scanner(System.in);
+        String s = "welcometojava";
+        int k = 3;
+        // String s = scan.next();
+        // int k = scan.nextInt();
+        scan.close();
 
-        // Create Locale for India with English language
-        Locale indiaLocale = new Locale("en", "IN");
-
-        // Format the payment for each country
-        NumberFormat usFormat = NumberFormat.getCurrencyInstance(Locale.US);
-        NumberFormat indiaFormat = NumberFormat.getCurrencyInstance(indiaLocale);
-        NumberFormat chinaFormat = NumberFormat.getCurrencyInstance(Locale.CHINA);
-        NumberFormat franceFormat = NumberFormat.getCurrencyInstance(Locale.FRANCE);
-
-        String us = usFormat.format(payment);
-        String india = indiaFormat.format(payment);
-        String china = chinaFormat.format(payment);
-        String france = franceFormat.format(payment);
-
-        // Print the formatted values
-        System.out.println("US: " + us);
-        System.out.println("India: " + india);
-        System.out.println("China: " + china);
-        System.out.println("France: " + france);
+        System.out.println(getSmallestAndLargest(s, k));
     }
 }
